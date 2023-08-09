0d7b32d44e4a89c16f80b8c4cce839af4ebeeb98
tia paola <133296841+IANELROBLOXIANO@users.noreply.github.com>
2023-08-08 20:09:05 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+// console.log es una palabra reservada
+console.log ("mi primer prollecto en la consola");
+
+var primer_nombre="ian yahel";
+var apellido="gutierrez vega";
+
+var nombre_completo=primer_nombre.concat(apellido);
+console.log(nombre_completo);
+
+var a= 10;
+var b= 5;
+
+var sumar= a + b;
+console.log (sumar);
+
+var restar= a - b;
+console.log (restar);
+
+var multiplicar= a * b;
+console.log (multiplicar);
+
+var dividir= a / b;
+console.log (dividir);
